153dffbb76ca9f4cb52e814de0d7e08be4e0408c
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2020-05-29 21:05:13 +0300

    update

--- post-processing/main.go
+++ post-processing/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"time"
 )
 
 func main() {
 	logFile := "./run.log"
-	fp, err := os.OpenFile(logFile, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0666)
+	fp, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
@@ -17,14 +16,8 @@ func main() {
 	headerLine := fmt.Sprintf("###### Run: %s\n", time.Now())
 	_, _ = fp.WriteString(headerLine)
 
-	argsLine := fmt.Sprintf("> Args: %s\n", strings.Join(os.Args, " "))
-	_, _ = fp.WriteString(argsLine)
-
-	envSlice := os.Environ()
-	for _, s := range envSlice {
-		envLine := fmt.Sprintf("> %s\n", s)
-		_, _ = fp.WriteString(envLine)
-	}
+	// TODO: list the S3 bucket key prefix and log results
+	// TODO: list reference directory
 
 	_, _ = fp.WriteString("\n\n")
 	_ = fp.Sync()
