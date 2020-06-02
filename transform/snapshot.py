e7e4dfa0ef19c437e3dba59b8f6d697d72b9a204
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2020-06-02 21:04:25 +0300

    refactor

--- transform/snapshot.py
+++ transform/snapshot.py
@@ -7,9 +7,10 @@ spark = get_spark()
 # 1. load delta table
 print(f"Load delta table from {cmd_args.delta_path}...")
 df = spark.read.format("delta").load(cmd_args.delta_path)
-df.show(10)
 
-# 2. export to parquet
+# 2. export to parquet snapshot
 print(f"Storing snapshot in {cmd_args.snapshot_path}...")
 df.write.mode("overwrite").parquet(cmd_args.snapshot_path)
+
+# 3. done
 print("Done!")
