9efebaaecde9ee7f97410848dd2d63653fb8a652
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2020-06-10 18:25:41 +0300

    scd type 2

--- Makefile
+++ Makefile
@@ -18,13 +18,14 @@ DELTA_BASE_STORAGE := "/Users/arkady/Projects/disney/spark_data/out"
 DELTA_LIBRARY_JAR := "/Users/arkady/Projects/tools/libs/delta-core_2.11-0.6.1.jar"
 
 ## working on single table
-TABLE_NAME := "dbo.WRKFLW_INSTNC"
-#TABLE_NAME := "dbo.test_changing_load"
+#TABLE_NAME := "dbo.WRKFLW_INSTNC"
+TABLE_NAME := "dbo.test_changing_load"
 #TABLE_NAME := "dbo.test_bulk_load"
 #TABLE_NAME := "dbo.WRKFLW_EVNT"
 TABLE_LOAD_PATH := "$(TABLE_BASE_STORAGE)/$(TABLE_NAME)"
 TABLE_CHANGES_PATH := "$(TABLE_BASE_STORAGE)/$(TABLE_NAME)__ct"
 TABLE_DELTA_PATH := "$(DELTA_BASE_STORAGE)/$(TABLE_NAME)__delta"
+TABLE_DELTA_SDC_PATH := "$(DELTA_BASE_STORAGE)/$(TABLE_NAME)__sdc"
 TABLE_SNAPSHOT_PATH := "$(DELTA_BASE_STORAGE)/$(TABLE_NAME)__snapshot"
 
 .PHONY: clean
@@ -52,6 +53,19 @@ changes:
 		./transform/changes.py \
 			--delta-library-jar $(DELTA_LIBRARY_JAR) \
 			--delta-path $(TABLE_DELTA_PATH) \
+			--load-path $(TABLE_LOAD_PATH) \
+			--changes-path $(TABLE_CHANGES_PATH) \
+			--snapshot-path $(TABLE_SNAPSHOT_PATH)
+
+.PHONY: changes_sdc
+changes_sdc:
+	@echo "### Processing incremental delta table changes..."
+	@spark-submit \
+		--master local[*] \
+		./transform/changes_scd.py \
+			--delta-library-jar $(DELTA_LIBRARY_JAR) \
+			--delta-path $(TABLE_DELTA_PATH) \
+			--delta-sdc-path $(TABLE_DELTA_SDC_PATH) \
 			--load-path $(TABLE_LOAD_PATH) \
 			--changes-path $(TABLE_CHANGES_PATH) \
 			--snapshot-path $(TABLE_SNAPSHOT_PATH)
---
+++ transform/changes_scd.py
@@ -0,0 +1,118 @@
+import sys
+from pyspark.sql.types import TimestampType, BooleanType, StringType
+from pyspark.sql.functions import col, lit
+
+from lib.args import get_args
+from lib.constants import CHANGES_METADATA_OPERATION, CHANGES_METADATA_TIMESTAMP
+from lib.metadata import get_batch_metadata, get_metadata_file_list
+from lib.spark import get_spark
+from lib.table import process_special_fields, get_delta_table
+
+cmd_args = get_args()
+spark = get_spark()
+
+# List "change" files
+print(f">>> Searching for batch metadata files in: {cmd_args.changes_path}...")
+dfm_files = get_metadata_file_list(cmd_args.changes_path)
+if not dfm_files:
+    print(">>> Nothing to-do, exiting...")
+    sys.exit(0)
+
+# Get batch metadata and validate columns
+print(f">>> Found {len(dfm_files)} batch metadata files, loading metadata...")
+batch = get_batch_metadata(
+    dfm_files=dfm_files,
+    src_path_override=cmd_args.changes_path
+)
+print(f">>> Metadata loaded, num_files={len(batch.files)}, records={batch.record_count}")
+if not batch.files:
+    raise Exception("Did not found any files to load..")
+
+if len(batch.primary_key_columns) > 1:
+    raise Exception("Composite primary keys not yet implemented!")
+
+if len(batch.primary_key_columns) == 0:
+    raise Exception("Batches without primary keys are not supported!")
+
+# Load batch
+print(f">>> Loading batch...")
+txt_files = spark.sparkContext.textFile(",".join(batch.files))
+batch_df = spark.read.json(txt_files, schema=batch.schema_batch)
+print(f">>> Collected: {batch_df.count()} changes before operation filtering...")
+
+# Post-process fields and filter out
+print(f">>> Collecting INSERT and UPDATE operations...")
+batch_df = process_special_fields(batch, batch_df)
+primary_key = batch.primary_key_columns[0]
+
+inserts_df = batch_df \
+    .filter(batch_df[CHANGES_METADATA_OPERATION].isin(["I"])) \
+    .orderBy(batch_df[CHANGES_METADATA_TIMESTAMP].asc()) \
+    .withColumn("_merge_key", lit(None).cast(StringType()))
+
+updates_df = batch_df \
+    .filter(batch_df[CHANGES_METADATA_OPERATION].isin(["U"])) \
+    .orderBy(batch_df[CHANGES_METADATA_TIMESTAMP].asc()) \
+    .withColumn("_merge_key", col(primary_key))
+
+print(f">>> Collected: UPDATES={updates_df.count()}, INSERTS={inserts_df.count()}")
+# staged_df = inserts_df \
+#     .selectExpr("NULL as _merge_key", "*") \
+#     .union(updates_df.selectExpr(f"{primary_key} as _merge_key", "*"))
+# 
+# staged_df.show(20, False)
+
+scd_schema = batch.schema_table
+scd_schema.add('current', BooleanType(), False)
+scd_schema.add('effective_date', TimestampType(), True)
+scd_schema.add('end_date', TimestampType(), True)
+
+inserts_df.show(20, False)
+updates_df.show(20, False)
+
+# Load delta table
+print(f">>> Loading SDC Type 2 delta table from {cmd_args.delta_sdc_path}...")
+scd_delta_table = get_delta_table(
+    spark=spark,
+    schema=batch.schema_table,
+    delta_library_jar=cmd_args.delta_library_jar,
+    delta_path=cmd_args.delta_sdc_path,
+)
+
+# grab only meaning columns, skip the rest
+value_map = {}
+for col in batch.columns:
+    dst = col['name']
+    if dst not in batch.metadata_columns:
+        src = f"s.{dst}"
+        value_map[dst] = src
+
+value_map.update({
+    'current': "true",
+    'effective_date': "s.header__timestamp",
+    'end_date': "null",
+})
+
+# Apply changes
+# @see: https://docs.databricks.com/delta/delta-update.html#scd-type-2-using-merge-notebook
+print(f">>> Applying changes to target delta table...")
+scd_delta_table \
+    .alias("t") \
+    .merge(inserts_df.alias("s"), f"t.{primary_key} = _merge_key") \
+    .whenNotMatchedInsert(values=value_map) \
+    .execute()
+
+scd_delta_table \
+    .alias("t") \
+    .merge(updates_df.alias("s"), f"t.{primary_key} = _merge_key") \
+    .whenMatchedUpdate(condition="t.current = true", set={
+        "current": "false",
+        "end_date": "s.header__timestamp",
+    }).execute()
+
+# temp
+scd_delta_table \
+    .toDF() \
+    .show(20, False)
+
+print(">>> Done!")
