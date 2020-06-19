44a7aa29c4a341387dc9aa4165c7cc1cba8eeef4
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2020-06-19 18:10:24 +0300

    snapshot without metadata fields

--- transform/hudi_snapshot.py
+++ transform/hudi_snapshot.py
@@ -1,6 +1,7 @@
 from lib.args import get_hudi_args
 from lib.spark import get_spark
 from lib.table import calculate_partitions
+from lib.constants import CHANGES_METADATA_FIELD_PREFIX
 
 cmd_args = get_hudi_args()
 spark = get_spark()
@@ -12,8 +13,9 @@ df = spark \
     .format("hudi") \
     .load(f"{cmd_args.hudi_path}/*")
 
-# filter Qlik header__ fields
-df.show(20)
+# Filter Qlik metadata fields
+df = df \
+    .select([c for c in df.columns if not c.startswith(CHANGES_METADATA_FIELD_PREFIX)])
 
 # Export to parquet snapshot
 partitions = calculate_partitions(spark=spark, df=df)
@@ -23,4 +25,5 @@ df.repartition(partitions) \
     .write \
     .mode("overwrite") \
     .parquet(cmd_args.snapshot_path)
+
 print(">>> Done!")
